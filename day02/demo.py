3e1eb5ea679ba9829d8ad9adb2a0189728b82d67
Gabor Szabo <gabor@szabgab.com>
2025-04-03 13:45:42 +0300

    update demo

--- day02/demo.py
+++ day02/demo.py
@@ -1,2 +1,12 @@
-greeting = "Hello, World"
-print(greeting)
+# greeting = "Hello, World"
+# print(greeting)
+
+# height = 1.75
+# weight = 68.0
+# area = height * weight
+# print("Area:", area)
+
+# name = "John Doe"
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+
