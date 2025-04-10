1836b3b0c4a8831150d176e26dffec247d392fda
Gabor Szabo <gabor@szabgab.com>
2025-04-10 14:06:30 +0300

    add

---
+++ day03/demo.py
@@ -0,0 +1,17 @@
+import math
+
+print(math.pi)
+
+x = 2
+print(x)
+print(y)
+y = 13
+print(42)
+
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
+# pick 3 random fruits
+import random
+random_fruits = random.sample(fruits, 3)
+print(random_fruits)
+
